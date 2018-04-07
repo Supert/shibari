1cd58569d1fc49c6d01d872b3a95ca766c5f1209
Alexey Korolev <doctor.irrational@gmail.com>
2018-04-07 14:57:44 +0500

    Strict text view.

---
+++ UI/StrictTextView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shibari.UI
+{
+    [RequireComponent(typeof(Text))]
+    public class StrictTextView : TextView
+    {
+        private BindableValueRestraint[] bindableValueTypes = new BindableValueRestraint[1]
+        {
+            new BindableValueRestraint(typeof(string))
+        };
+
+        public override BindableValueRestraint[] BindableValueRestraints { get { return bindableValueTypes; } }
+    }
+}
